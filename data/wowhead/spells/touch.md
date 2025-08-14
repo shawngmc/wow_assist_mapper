c24410a2938cc1c556749eb89fb4951704752ec8
Shawn McNaughton <shawngmc@gmail.com>
2025-08-14 00:54:59 -0400

    Initial implementation

---
+++ data/wowhead/spells/touch.md
@@ -0,0 +1,1 @@
+# Folder for scraped spells
--- src/wow_assist_mapper/skeleton.py
+++ src/wow_assist_mapper/skeleton.py
@@ -1,3 +1,4 @@
+
 """
 This is a skeleton file that can serve as a starting point for a Python
 console script. To run this script uncomment the following lines in the
@@ -22,7 +23,17 @@ References:
 
 import argparse
 import logging
+import os
+import re
 import sys
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+from pprint import pprint
+from enum import Enum
+
+import requests
 
 from wow_assist_mapper import __version__
 
@@ -33,6 +44,25 @@ __license__ = "MIT"
 _logger = logging.getLogger(__name__)
 
 
+class OutputFormat(Enum):
+    RAW = "raw"
+    TEXT = "text"
+
+class PlayerClass(Enum):
+    DEATH_KNIGHT = "Death_knight"
+    DEMON_HUNTER = "Demon_hunter"
+    DRUID = "Druid"
+    EVOKER = "Evoker"
+    HUNTER = "Hunter"
+    MAGE = "Mage"
+    MONK = "Monk"
+    PALADIN = "Paladin"
+    PRIEST = "{riest"
+    ROGUE = "Rogue"
+    SHAMAN = "Shaman"
+    WARLOCK = "Warlock"
+    WARRIOR = "Warrior"
+
 # ---- Python API ----
 # The functions defined in this section can be imported by users in their
 # Python scripts/interactive interpreter, e.g. via
@@ -40,27 +70,12 @@ _logger = logging.getLogger(__name__)
 # when using this Python module as a library.
 
 
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
 
 
 # ---- CLI ----
 # The functions defined in this section are wrappers around the main Python
 # API allowing them to be called directly from the terminal as a CLI
 # executable/script.
-
 
 def parse_args(args):
     """Parse command line parameters
@@ -72,13 +87,13 @@ def parse_args(args):
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
+    parser = argparse.ArgumentParser(description="Map Assist/Single-Button")
     parser.add_argument(
         "--version",
         action="version",
         version=f"wow_assist_mapper {__version__}",
     )
-    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
+    # parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
     parser.add_argument(
         "-v",
         "--verbose",
@@ -95,6 +110,30 @@ def parse_args(args):
         action="store_const",
         const=logging.DEBUG,
     )
+    parser.add_argument(
+        "-c",
+        "--class",
+        type=str,
+        dest="charclass",
+        choices=[e.value for e in PlayerClass],
+        help=f"Class to Examine ({', '.join(e.value for e in PlayerClass)})",
+        required=True
+    )
+    parser.add_argument(
+        "-s",
+        "--spec",
+        type=str,
+        help="Spec to examine (e.g. Balance, Frost, BeastMastery,etc. - if there's a space, use CamelCase with an underscore )",
+        required=True
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        type=str,
+        choices=[e.value for e in OutputFormat],
+        help=f"Output format ({', '.join(e.value for e in OutputFormat)})",
+        required=True
+    )
     return parser.parse_args(args)
 
 
@@ -109,9 +148,24 @@ def setup_logging(loglevel):
         level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
 
+def check_spell_data(spellID):
+    spell_data_dir = "data/wowhead_spellscrape"
+    spell_file = f'{spell_data_dir}/spell_{spellID}.json'
+    if not os.path.exists(spell_file):
+        _logger.info(f"Spell data for {spellID} does not exist, scraping from Wowhead")
+        spell_data = scrape_spell_from_wowhead(int(spellID))
+        if spell_data:
+            with open(spell_file, 'w', encoding='utf-8') as f:
+                json.dump(spell_data, f, ensure_ascii=False, indent=4)
+            return spell_data
+    else:
+        _logger.info(f"Spell data for {spellID} already exists, loading from {spell_file}")
+        with open(spell_file, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
 
 def main(args):
-    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+    """Wrapper allowing CLI usage
 
     Instead of returning the value from :func:`fib`, it prints the result to the
     ``stdout`` in a nicely formatted message.
@@ -122,10 +176,188 @@ def main(args):
     """
     args = parse_args(args)
     setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print(f"The {args.n}-th Fibonacci number is {fib(args.n)}")
+    _logger.debug("Starting...")
+
+    condition_map = pd.read_csv('data/ConditionTypeMap.csv', quoting=0, engine='python', quotechar='"')
+    classes, condition_types = parse_and_build_db_dumps()
+
+    if args.format == OutputFormat.RAW.value:
+        rotation = find_rotation(classes, args.charclass, args.spec)
+        pprint(rotation)
+    elif args.format == OutputFormat.TEXT.value:
+        rotation = get_plaintext_rotation(classes, args.charclass, args.spec, condition_map)
+        print(rotation)
+
     _logger.info("Script ends here")
 
+# def get_file_types_in_cascs():
+#     with open('data/casc_filelist/community-listfile.csv', "r") as file:
+#         lines = file.readlines()
+#     types = {}
+#     for line in lines:
+#         root, file_type = os.path.splitext(line)
+#         file_type = file_type.lower().strip()
+#         if file_type not in types:
+#             types[file_type] = 0
+#         types[file_type] += 1
+#     return types
+
+
+def parse_and_build_db_dumps():
+    chr_specs = pd.read_csv('data/csv_table/ChrSpecialization.11.2.0.62493.csv', quoting=0, engine='python', quotechar='"')
+    chr_classes = pd.read_csv('data/csv_table/ChrClasses.11.2.0.62493.csv', quoting=0, engine='python', quotechar='"')
+    assisted_combat = pd.read_csv('data/csv_table/AssistedCombat.11.2.0.62493.csv', quoting=0, engine='python', quotechar='"')
+    assisted_combat_rule = pd.read_csv('data/csv_table/AssistedCombatRule.11.2.0.62493.csv', quoting=0, engine='python', quotechar='"')
+    assisted_combat_step = pd.read_csv('data/csv_table/AssistedCombatStep.11.2.0.62493.csv', quoting=0, engine='python', quotechar='"')
+
+    # Map character classes
+    classes = {}
+    for index, row in chr_classes.iterrows():
+        _logger.info(f"Class Row {index}: {row.to_dict()}")
+        class_key = row['Name_lang']
+        classes[class_key] = {}
+        classes[class_key]["raw_data"] = row.to_dict()
+        classes[class_key]["ID"] = int(row['ID'])
+        classes[class_key]["Name"] = row['Name_lang']
+        classes[class_key]["specs"] = {}
+
+    # Find the class that matches the spec ID
+    # and add the spec to that class
+    specs = {}
+    for index, row in chr_specs.iterrows():
+        _logger.info(f"Spec Row {index}: {row.to_dict()}")
+        spec_key = row['Name_lang']
+        specs[spec_key] = {}
+        specs[spec_key]["raw_data"] = row.to_dict()
+        specs[spec_key]["ID"] = int(row['ID'])
+        specs[spec_key]["Name"] = row['Name_lang']
+        for player_class, class_data in classes.items():
+            if class_data["ID"] == row["ClassID"]:
+                class_data["specs"][spec_key] = specs[spec_key]
+
+    # Add plans to specs
+    assist_plans = {}
+    for index, row in assisted_combat.iterrows():
+        _logger.info(f"Assisted Combat Row {index}: {row.to_dict()}")
+        assist_key = row['ID']
+        assist_plans[assist_key] = {}
+        assist_plans[assist_key]["steps"] = {}
+        assist_plans[assist_key]["ID"] = int(row['ID'])
+        for player_spec, spec_data in specs.items():
+            if spec_data["ID"] == row["ChrSpecializationID"]:
+                spec_data["assist_plan"] = assist_plans[assist_key]
+
+    # Add steps to plans
+    assist_steps = {}
+    for index, row in assisted_combat_step.iterrows():
+        _logger.info(f"Assisted Combat Step Row {index}: {row.to_dict()}")
+        step_key = int(row['ID'])
+        assist_key = row['AssistedCombatID']
+        assist_steps[step_key] = {}
+        assist_steps[step_key]["ID"] = int(row['ID'])
+        assist_steps[step_key]["SpellID"] = int(row['SpellID'])
+        # Check if spell data exists, if not scrape it
+        spell_data = check_spell_data(assist_steps[step_key]["SpellID"])
+        assist_steps[step_key]["SpellData"] = spell_data
+        assist_steps[step_key]["SpellName"] = spell_data['name']
+        assist_steps[step_key]["OrderIndex"] = int(row['OrderIndex'])
+        assist_steps[step_key]["rules"] = {}
+        assist_plans[assist_key]["steps"][int(row['OrderIndex'])] = assist_steps[step_key]
+
+    # Add rules to steps
+    assist_rules = {}
+    condition_types = {}
+    for index, row in assisted_combat_rule.iterrows():
+        _logger.info(f"Assisted Combat Rule Row {index}: {row.to_dict()}")
+        rule_key = int(row['ID'])
+        assist_step_key = int(row['AssistedCombatStepID'])
+        if int(row['Field_11_1_7_60520_002']) != 0:
+            _logger.warning(f"Field Field_11_1_7_60520_002 is non-zero: {row['Field_11_1_7_60520_002']}")
+            _logger.warning(row.to_dict())
+        assist_rules[rule_key] = {}
+        assist_rules[rule_key]["ID"] = int(row['ID'])
+        assist_rules[rule_key]["OrderIndex"] = int(row['OrderIndex'])
+        assist_rules[rule_key]["raw"] = row.to_dict()
+        assist_steps[assist_step_key]["rules"][int(row['OrderIndex'])] = assist_rules[rule_key]
+        if not int(row['ConditionType']) in condition_types:
+            condition_types[int(row['ConditionType'])] = f"{row['ConditionValue1']},{row['ConditionValue2']},{row['ConditionValue3']}"
+    return classes, condition_types
+
+def get_plaintext_rotation(classes, class_name, spec_name, condition_map):
+    """Get the rotation for a given class and spec in plaintext format."""
+    lines = []
+    rotation = find_rotation(classes, class_name, spec_name)
+    for index, step in enumerate(rotation):
+        spell_name = step['SpellName']
+        lines.append(f"{index}: Spell: {spell_name}")
+        for rule_index, rule in step['rules'].items():
+            # Select conditionmap based on condition type
+            cond_type = rule['raw'].get('ConditionType')
+            rule_props = condition_map[condition_map['Type'] == cond_type]
+
+            # Prep vars, including resolving spells
+            spell = f"'{spell_name}'"
+            arg1 = rule['raw'].get('ConditionValue1')
+            if rule_props.iloc[0]['Value1'].lower().startswith("spell"):
+                arg1 = f"'{check_spell_data(arg1)['name']}'"
+            arg2 = rule['raw'].get('ConditionValue2')
+            if rule_props.iloc[0]['Value2'].lower().startswith("spell"):
+                arg2 = f"'{check_spell_data(arg2)['name']}'"
+            arg3 = rule['raw'].get('ConditionValue3')
+            if rule_props.iloc[0]['Value3'].lower().startswith("spell"):
+                arg3 = f"'{check_spell_data(arg3)['name']}'"
+            template = rule_props.iloc[0]['Description']
+            lines.append("    " + eval('f"' + template + '"'))
+    return "\n".join(lines)
+
+def find_rotation(classes, class_name, spec_name):
+    """Get the rotation for a given class and spec in plaintext format."""
+    rotation = []
+    class_data = classes.get(class_name)
+    if class_data:
+        clean_spec_name = re.sub(r'(?<!^)(?=[A-Z])', ' ', spec_name)
+        spec_data = class_data["specs"].get(clean_spec_name)
+        if spec_data:
+            assist_plan = spec_data.get("assist_plan")
+            if assist_plan:
+                for step in assist_plan.get("steps", {}).values():
+                    step_object = {}
+                    step_object["SpellName"] = step["SpellName"]
+                    step_object["rules"] = step["rules"]
+                    rotation.append(step_object)
+        else:
+            _logger.error(f"Spec not found: {spec_name}")
+    return rotation
+
+def scrape_spell_from_wowhead(spellID):
+    """Scrape spell information from Wowhead."""
+    url = f"https://www.wowhead.com/spell={spellID}"
+    response = requests.get(url)
+    # Enforce sleep to not overload the server
+    time.sleep(2)
+    if response.status_code == 200:
+        html_string = response.text
+
+        soup = BeautifulSoup(html_string, 'html.parser')
+        spell_name = soup.find("h1", class_="heading-size-1").text.strip()
+        
+        # Find the icon name
+        # They inject via JS; luckily, the JS format looks predictable enough to get the name
+        # Find the first instance of '"icon": ' in the raw HTML, then take until next double quote
+        icon_name = None
+        for line in html_string.splitlines():
+            idx = line.find("\"icon\":\"")
+            if idx != -1:
+                icon_name = line[idx+8:]
+                icon_name = icon_name.split("\"")[0]
+                icon_url = f"https://wow.zamimg.com/images/wow/icons/large/{icon_name}.jpg"
+        return {
+            "id": spellID,
+            "name": spell_name,
+            "icon_name": icon_name,
+            "icon_url": icon_url
+        }
+    return None
 
 def run():
     """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
